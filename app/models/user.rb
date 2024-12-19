31173b1893dfd10f6b43d450fd96eb2be26c0577
uw819kt <miya0arima@gmail.com>
2024-12-19 14:24:59 +0900

    colect_5

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,18 +1,12 @@
 class User < ApplicationRecord
   has_many :tasks, dependent: :destroy
-  
-  with_options presence: true do
-  validates :name, presence: { message: 'を入力してください' }
-  validates :name,length: { maximum: 30 }
-  validates :email, presence: { message: 'を入力してください' }
-  validates :email, length: { maximum: 255 }
-  validates :email, uniqueness: { message: 'は既に使用されています' }
-  validates :email, format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i }
+
+  validates :name, presence: true, length: { maximum: 30 }
+  validates :email, presence: true, length: { maximum: 255 }, uniqueness: true, format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i }
   before_validation { email.downcase! }
   has_secure_password
-  validates :password, presence: { message: 'を入力してください' }
   validates :password, length: { minimum: 6,
-             too_short: "パスワードは6文字以上で入力してください",            
+             too_short: "は6文字以上で入力してください",            
             }
 
 end
