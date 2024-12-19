b7c990890cdd53464d56b5a7079e4249eb2847bc
uw819kt <miya0arima@gmail.com>
2024-12-19 21:26:24 +0900

    collect_7

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,12 +1,22 @@
 class User < ApplicationRecord
   has_many :tasks, dependent: :destroy
 
-  validates :name, presence: true, length: { maximum: 30 }
-  validates :email, presence: true, length: { maximum: 255 }, uniqueness: true, format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i }
+  validates :name, presence: true
+  validates :name, length: { maximum: 30 }
+  validates :email, presence: true 
+  validates :email, length: { maximum: 255 } 
+  validates :email, uniqueness: { message: "はすでに使用されています" }
+  validates :email, format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i }
   before_validation { email.downcase! }
   has_secure_password
   validates :password, length: { minimum: 6,
              too_short: "は6文字以上で入力してください",            
             }
 
+  
+  def password_confirmation
+    if password != password_confirmation
+      errors.add(:password_confirmation, 'とパスワードの入力が一致しません')
+    end
+  end
 end
